5839052a10988b7867acb82beafe63e11a719d4d
Ali Bayat <ali.bayat77@yahoo.com>
2021-10-14 16:59:56 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  let nav = document.querySelector('.navigation')
+  let main = document.querySelector('.main')
+  nav.classList.toggle('active')
+  main.classList.toggle('active')
+}
